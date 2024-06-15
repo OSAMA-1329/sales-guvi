d6da1c4352f930338790a79ab3f8fbc5fbdedf8b
OSAMA-1329 <usamausmah@gmail.com>
2024-06-15 22:21:06 +0530

    first commmit

---
+++ src/data/tableData.js
@@ -0,0 +1,74 @@
+export const tableData=[
+    {
+        id:'#AHGA68',
+        date:'23/09/2022',
+        customer:'dummy name 1',
+        payableAmount:'$100',
+        paidAmount:'$000',
+        due:'$000'
+    },
+    {
+        id: '#AHGA68',
+        date: '23/09/2022',
+        customer: 'dummy name 2',
+        payableAmount: '$100',
+        paidAmount: '$000',
+        due: '$000'
+    },
+    {
+        id: '#AHGA68',
+        date: '23/09/2022',
+        customer: 'dummy name 3',
+        payableAmount: '$100',
+        paidAmount: '$000',
+        due: '$000'
+    },
+    {
+        id: '#AHGA68',
+        date: '23/09/2022',
+        customer: 'dummy name 4',
+        payableAmount: '$100',
+        paidAmount: '$000',
+        due: '$000'
+    },
+    {
+        id: '#AHGA68',
+        date: '23/09/2022',
+        customer: 'dummy name 5',
+        payableAmount: '$100',
+        paidAmount: '$000',
+        due: '$000'
+    },
+    {
+        id: '#AHGA68',
+        date: '23/09/2022',
+        customer: 'Dummy  Name',
+        payableAmount: '$100',
+        paidAmount: '$000',
+        due: '$000'
+    },
+    {
+        id: '#AHGA68',
+        date: '23/09/2022',
+        customer: 'Dummy  Name',
+        payableAmount: '$100',
+        paidAmount: '$000',
+        due: '$000'
+    },
+    {
+        id: '#AHGA68',
+        date: '23/09/2022',
+        customer: 'Dummy  Name',
+        payableAmount: '$100',
+        paidAmount: '$000',
+        due: '$000'
+    },
+    {
+        id: '#AHGA68',
+        date: '23/09/2022',
+        customer: 'Dummy  Name',
+        payableAmount: '$100',
+        paidAmount: '$000',
+        due: '$000'
+    }
+]
